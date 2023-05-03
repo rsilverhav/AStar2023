19e81c261c7cd2463bd77f3c29564b0df14ecc66
Robin Silverhav <robinsilverhav@gmail.com>
2023-05-03 17:05:28 +0200

    better terminal output for map

--- src/TerminalMapRenderer.cpp
+++ src/TerminalMapRenderer.cpp
@@ -1,10 +1,24 @@
 #include "TerminalMapRenderer.h"
 
 void TerminalMapRenderer::renderMap(const std::vector<std::vector<int>> &map) {
-  for (auto row : map) {
-    for (auto col : row) {
-      std::cout << col;
-    }
-    std::cout << "\n";
+  for (int i = 0; i < map.at(0).size() + 2; i++) {
+    std::cout << "-";
   }
+  std::cout << "\n";
+  for (auto row : map) {
+    std::cout << "|";
+    for (auto col : row) {
+      switch (col) {
+      case 1:
+        std::cout << "#";
+      default:
+        std::cout << " ";
+      }
+    }
+    std::cout << "|\n";
+  }
+  for (int i = 0; i < map.at(0).size() + 2; i++) {
+    std::cout << "-";
+  }
+  std::cout << "\n";
 }
