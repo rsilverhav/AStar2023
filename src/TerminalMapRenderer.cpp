d18ebc65887f84c9667290ff4cda9ccc945ab7f0
Robin Silverhav <robinsilverhav@gmail.com>
2023-05-04 14:56:08 +0200

    added todo comment about deriving key to map

--- src/TerminalMapRenderer.cpp
+++ src/TerminalMapRenderer.cpp
@@ -17,12 +17,13 @@ void TerminalMapRenderer::renderMap(const std::vector<std::vector<int>> &map,
   for (int i = 0; i < map.at(0).size() + 2; i++) {
     std::cout << "-";
   }
+
   std::cout << "\n";
   for (int y = 0; y < map[0].size(); y++) {
-    auto row = map[y];
     std::cout << "|";
     for (int x = 0; x < map[y].size(); x++) {
-      auto col = row[x];
+      auto col = map[y][x];
+      // TODO: this is bad, should not derive the key in two different places
       std::string key = std::to_string(x) + "," + std::to_string(y);
       if (col == 1) {
         std::cout << "#";
