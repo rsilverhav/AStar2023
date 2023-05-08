6a36c0b169b7796a9965354a006067b21035c254
Robin Silverhav <robinsilverhav@gmail.com>
2023-05-08 14:13:45 +0200

    added more walls to map

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,15 @@ int main() {
   auto gameMap = GameMap(width, height);
 
   gameMap.setTileType({3, 3}, MapTileType::wall);
+  gameMap.setTileType({4, 3}, MapTileType::wall);
+  gameMap.setTileType({5, 3}, MapTileType::wall);
+  gameMap.setTileType({6, 3}, MapTileType::wall);
+  gameMap.setTileType({7, 3}, MapTileType::wall);
   gameMap.setTileType({3, 4}, MapTileType::wall);
   gameMap.setTileType({3, 5}, MapTileType::wall);
+  gameMap.setTileType({4, 5}, MapTileType::wall);
+  gameMap.setTileType({4, 6}, MapTileType::wall);
+  gameMap.setTileType({5, 6}, MapTileType::wall);
 
   std::unique_ptr<MapRenderer> mapRenderer =
       std::make_unique<TerminalMapRenderer>();
